d889e53232620cb8bebde36a3e9e3255890f0b71
DaniFdz <danifernandezzzzzz@gmail.com>
2024-01-28 20:37:38 +0100

    Hero page, and collections logic definition

---
+++ src/components/Header.tsx
@@ -0,0 +1,123 @@
+import { useState } from "preact/compat";
+import MenuIcon from "@/components/icons/Menu";
+import LinkedInIcon from "./icons/LinkedIn";
+import InstagramIcon from "./icons/Instagram";
+import { collections } from "@/utils/collections";
+
+export default function Navbar() {
+  const [collectionsOpen, setCollectionsOpen] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <>
+      <nav className="absolute top-0 left-0 z-50 w-dvw flex flex-wrap items-center justify-between backdrop-blur-sm mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between">
+            <div class="flex flex-row gap-2 items-center py-3">
+              <a
+                className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                href="/"
+              >
+                GOON
+              </a>
+              <a
+                className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                href="#"
+                onMouseEnter={() => setCollectionsOpen(true)}
+                onClick={() => setCollectionsOpen(!collectionsOpen)}
+              >
+                Collections
+              </a>
+            </div>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none md:hidden"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <MenuIcon/>
+            </button>
+          <ul className="hidden md:flex flex-row items-center list-none ml-auto gap-2">
+              <li className="nav-item">
+                <a
+                  className="px-2 py-1 flex items-center text-xs gap-1 uppercase font-bold leading-snug text-white rounded-full hover:opacity-75 hover:scale-110 hover:bg-gray-200/30 transform transition-all duration-300"
+                  href="https://www.instagram.com/gooncouture/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <InstagramIcon/>
+                  Instagram
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-2 py-1 flex items-center text-xs gap-1 uppercase font-bold leading-snug text-white rounded-full hover:opacity-75 hover:scale-110 hover:bg-gray-200/30 transform transition-all duration-300"
+                  href="https://www.linkedin.com/in/laurana-corrales-aldea-64b1232aa/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <LinkedInIcon/>
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div
+            className={
+              "flex-grow items-center md:hidden" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+          <ul className="flex flex-col list-none justify-center gap-2">
+            <li className="nav-item">
+                <a
+                  className="px-2 py-1 flex items-center text-xs gap-1 uppercase font-bold leading-snug text-white rounded-full hover:opacity-75 hover:scale-110 hover:bg-gray-200/30 transform transition-all duration-300"
+                  href="https://www.instagram.com/gooncouture/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <InstagramIcon/>
+                  Instagram
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-2 py-1 flex items-center text-xs gap-1 uppercase font-bold leading-snug text-white rounded-full hover:opacity-75 hover:scale-110 hover:bg-gray-200/30 transform transition-all duration-300"
+                  href="https://www.linkedin.com/in/laurana-corrales-aldea-64b1232aa/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <LinkedInIcon/>
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div
+            className={"mx-14 flex-grow items-center" +
+              (collectionsOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+            onMouseLeave={() => setCollectionsOpen(false)}
+          >
+          <ul className="flex flex-col list-none justify-center gap-2">
+            {collections.map((collection) => (
+              
+              <li className="nav-item px-2 py-1 rounded-full hover:opacity-75 hover:scale-110 hover:bg-gray-200/30 transform transition-all duration-300">
+                  <a
+                    className="text-xs uppercase font-bold leading-snug text-white"
+                    href={`/collections/${collection.name}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {collection.name}
+                  </a>
+              </li>
+            ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
