ce57450bd646b3bcb96ecadcef052464e939942f
DaniFdz <danifernandezzzzzz@gmail.com>
2024-01-31 00:23:30 +0100

    Responsive collections working

--- src/components/LayoutGrid.tsx
+++ src/components/LayoutGrid.tsx
@@ -1,14 +1,22 @@
 import type { Item } from "@/types/collections";
 
-export default function LayoutGrid({ items }: { items: Item[] }) {
-    <div className="mt-15 w-[90dvw] mx-auto flex flex-row flex-wrap z-10">
-        {items.map((item, i) => (
-            <img
-                key={i}
-                src={item.image}
-                alt={item.name}
-                className="max-w-[30vw] max-h-[30vw] w-full h-full"
-            />
-        ))}
-    </div>
+export default function LayoutGrid({ items, title }: { items: Item[], title: string }) {
+    return <section className="pt-20 min-h-dvh w-[90vw] mx-auto flex flex-col items-center justify-center">
+        <h2 className="text-4xl text-black [font-family: Montserrat, sans-serif]">{title}</h2>
+        <ul className="flex flex-col md:flex-row items-center [width:90vw] overflow-x-auto gap-2.5">
+            {items.map((item, i) => (
+                <li className="flex flex-col justify-center items-center gap-3 p-3">
+                    <h3 className="text-lg [font-family: Montserrat, sans-serif]">
+                        {item.name}
+                    </h3>
+                    <img
+                        key={i}
+                        src={item.image}
+                        alt={item.name}
+                    className="w-11/12 max-h-max h-max md:[height:60dvh] md:max-w-max md:w-max rounded-lg"
+                    />
+                </li>
+            ))}
+        </ul>
+    </section>
 }
